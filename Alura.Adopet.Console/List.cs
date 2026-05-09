befad1d83c41a9485b0697e26584c2fd025eda8c
Rodrigo Abed <rodrigo.abed94@gmail.com>
2026-05-08 22:15:40 -0300

    ajuste de nomenclatura de variaveis e extração de classes.

---
+++ Alura.Adopet.Console/List.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console
+{
+    internal class List
+    {
+        // cria instância de HttpClient para consumir API Adopet
+        HttpClient client;
+
+        public List()
+        {
+            this.client = ConfiguraHttpClient("http://localhost:5057");
+        }
+        public async Task ListarPetAsync()
+        {
+            var pets = await ListPetsAsync();
+            foreach (var pet in pets)
+            {
+                System.Console.WriteLine(pet);
+            }
+        }
+
+        async Task<IEnumerable<Pet>?> ListPetsAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("pet/list");
+            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
+        }
+
+        HttpClient ConfiguraHttpClient(string url)
+        {
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.BaseAddress = new Uri(url);
+            return _client;
+        }
+    }
+}
